dd2c7469fe15446391a7109383b2e2d8e9f293e4
mylesglass <mylesalexanderglass@gmail.com>
2014-08-23 21:39:09 +1200

    Init
    From Neal

---
+++ html/src/com/ld/banana/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ld.banana.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ld.banana.BananaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BananaGame();
+        }
+}
